7b010cd9f81a156f673c5dee20e49506f979d2a8
unknown <timur.learn3@gmail.com>
2026-02-02 14:37:52 +0300

    Исправил опечатку в student/views.py

--- student/views.py
+++ student/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from cook.models import dishes
+# from cook.models import dishes
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "student/index.html")
 
 def menu(request: HttpRequest) -> HttpResponse:
-    context = {"dishes" : dishes.objects.all()}
+    context = {"dishes" : ["Nothing here"]}
     return render(request, "student/menu.html", context)
 
 def allergy(request: HttpRequest) -> HttpResponse:
