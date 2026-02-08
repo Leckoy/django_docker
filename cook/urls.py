28bd959c16087da99833c60d2ff683527d61e0e8
Leckoy <Leckoy700@gmail.com>
2026-02-08 14:56:29 +0300

    переделал из API в норм def ингредиенты и блюда

--- cook/urls.py
+++ cook/urls.py
@@ -1,21 +1,13 @@
 from django.urls import path
 from .views import *
 from . import views
+
 urlpatterns = [
 
+path('ingredient/', views.IngredientUse, name='ingredient'),
 
-path('ingredients/', IngredientUseAPI.as_view(), name='ingredient-dashboard'),
-
-path('ingredients/<int:pk>/use/', IngredientUseAPI.as_view(), name='ingredient-use'),
-
-path('dish/<int:pk>', StockCookDishAPI.as_view(), name='cook-dashboard'),
-
-path('stock/<int:pk>/use/', StockCookDishAPI.as_view(), name='dish-cook'),
 path('create/', views.CreateMenu, name='create_menu'),
 
-
-
-path("products/", fproducts),
-path("dishes/", fdishes),
+path("dishes/", views.DishAdd, name='dish'),
 
 ]
